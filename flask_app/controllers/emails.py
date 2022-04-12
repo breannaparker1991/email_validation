29d3b50f1f902235866b4265a70e48b1596479c8
bree parker <breannaparker1991@gmail.com>
2022-04-11 23:27:58 -0600

    prelim

--- flask_app/controllers/emails.py
+++ flask_app/controllers/emails.py
@@ -1,7 +1,31 @@
-from crypt import methods
 from flask_app import app
 from flask import redirect, request, render_template
+from flask_app.models.email import Email
 
-@app.route('/', methods="POST")
+@app.route('/')
 def index():
   return render_template("index.html")
+
+@app.route('/new', methods=["POST"])
+def new():
+  if not Email.validate(request.form):
+    return redirect('/')
+  Email.create(request.form)
+  return redirect('/show')
+
+  # data = {
+  #   "email": request.form['email']
+  # }
+  # Email.create(data)
+
+@app.route('/show')
+def show():
+  return render_template("show.html", email = Email.show())
+
+@app.route('/delete/<int:id>')
+def delete(id):
+  data = {
+    "id" : id
+  }
+  Email.delete(data)
+  return redirect('/show')
--- flask_app/models/email.py
+++ flask_app/models/email.py
@@ -1,3 +1,10 @@
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from unittest import result
+from winreg import QueryInfoKey
+from wsgiref import validate
+from flask import flash
+import re
+from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
 
 class Email:
@@ -8,3 +15,43 @@ class Email:
     self.updated_at = data['updated_at']
     self.created_at = data['created_at']
 
+  @classmethod
+  def create(cls,data):
+    query = "INSERT INTO email_validation(name, created_at) VALUES (%(email)s, NOW())"
+    result = connectToMySQL(cls.db).query_db(query,data)
+    return result
+
+  @classmethod
+  def show(cls):
+    query = "SELECT * FROM email_validation"
+    result = connectToMySQL(cls.db).query_db(query)
+    print(result)
+    email = []
+    for e in result:
+      email.append(cls(e))
+    return email
+  
+  @classmethod
+  def delete(cls,data):
+    query = "DELETE FROM email_validation WHERE id = %(id)s;"
+    return connectToMySQL(cls.db).query_db(query, data)
+  
+  @staticmethod
+  def validate(email):
+    validate = True
+    query = "SELECT * FROM email_validation WHERE email = %(email)s"
+    results = connectToMySQL(Email.db).query_db(query,email)
+    if len(results) >= 1:
+      flash("Email is already taken!")
+      validate = False
+    if not EMAIL_REGEX.match(email['email']):
+      flash('Invalid Email')
+      validate = False
+    return validate
+    
+    # validate = True
+    # if not EMAIL_REGEX.match(email['email']):
+    #   flash("EMAIL NOT VALID!!!")
+    #   validate = False
+    # return validate
+  
